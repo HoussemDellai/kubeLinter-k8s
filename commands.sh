ba6476291b6845803b1ee394bd5edc6e34db52b9
Houssem Dellai <houssem.dellai@live.com>
2021-03-07 09:38:17 +0100

    editing readme

--- commands.sh
+++ commands.sh
@@ -1,8 +1,21 @@
-kube-linter lint pod.yaml
+# Install KubeLinter as a CLI tool or run it inside a Docker container.
+# src: https://docs.kubelinter.io/#/
 
+# Scan Kubernetes YAML manifest files
+kube-linter lint cd k8s-yaml-sample/pod.yaml
 
 # Create a new sample Helm chart:
 helm create helm-chart-sample
 
 # Lint the helm chart:
 kube-linter lint helm-chart-sample/
+
+# Run KubeLinter in Docker container (sample command):
+docker run -v /path/to/files/you/want/to/lint:/dir 
+           -v /path/to/config.yaml:/etc/config.yaml 
+           stackrox/kube-linter 
+           lint /dir 
+           --config /etc/config.yaml
+
+# KubeLinter list of checks: 
+# https://docs.kubelinter.io/#/generated/checks
